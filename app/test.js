ee614b0bd6f812cd270164302735f8347ccc77f1
René Viering <rene.viering@gmail.com>
2016-08-22 22:30:33 +0200

    Add karma test infrastructure with es2015 code coverage

---
+++ app/test.js
@@ -0,0 +1,5 @@
+function add() {
+	return 12;
+}
+
+export default add;
---
+++ app/test.spec.js
@@ -0,0 +1,7 @@
+import add from './test.js';
+
+describe('foo', function() {
+	it('bar', function() {
+		expect(add()).to.equal(12);
+	});
+});
